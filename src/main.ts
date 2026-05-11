cff6c95a8885b05ea91cb6fe5ccc1a3d46dbdebd
Linh <22520780@gm.uit.edu.vn>
2026-05-11 21:38:07 +0700

    feat: initialize NestJS application with global configuration and exception handling

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  app.use(cookieParser());
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //Tự động loại bỏ các field không khai báo trong DTO.
+      forbidNonWhitelisted: true, //Không chỉ xoá field lạ mà còn throw error.
+      transform: true, //Tự động transform payload sang instance của DTO và convert type.
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Auth API')
+    .setDescription('Complete authentication system')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = configService.get<number>('PORT') ?? 3000;
+
+  await app.listen(port);
+
+  console.log(`Application is running on http://localhost:3000`);
+  console.log(`Swagger docs at http://localhost:${port}/api/docs`);
+}
+bootstrap();
